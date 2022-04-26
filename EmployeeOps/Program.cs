7dbd0d5bdb472eebb8baa6d641645d7b28644886
Ganeshp007 <potdart40.gp@gmail.com>
2022-04-26 22:53:02 +0530

    Added: UC1_EmpPresentOrNot

---
+++ EmployeeOps/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int Is_Fulltime = 1;
+Random obj=new Random();
+int empcheck=obj.Next(0,2);//it will generate 0 and 1 only bcoz its limit is n-1 ie 2-1=1
+
+Console.WriteLine("----- Welcome to Employee Management System -----\n");
+Console.ReadKey();
+
+if(empcheck == Is_Fulltime)
+{
+    Console.WriteLine("Employee is Present");
+}
+else
+{
+    Console.WriteLine("Employee is Absent");
+}
+
