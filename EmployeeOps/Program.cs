ded55d8c8eb7597b53337dcd37bab6c1b4b36057
Ganeshp007 <potdart40.gp@gmail.com>
2022-04-27 00:56:22 +0530

    Added: UC5_EmpwageFor20days

--- EmployeeOps/Program.cs
+++ EmployeeOps/Program.cs
@@ -4,39 +4,43 @@ class Exception
 public const int Is_Fulltime = 1;
 public const int Is_Parttime = 2;
 public const int Emp_Rate_Hr = 20;
+public const int Working_Days = 20;
 
    static void Main(String[] agrs)
    
    {
-
-            int Emp_Hr = 0;
-            int Emp_wage = 0;
-            Random obj = new Random();
-            int empcheck = obj.Next(0, 3); //it will generate 0 and 1 only bcoz its limit is n-1 ie 2-1=1
-
             Console.WriteLine("----- Welcome to Employee Management System -----\n");
             Console.ReadKey();
 
-            //In this program we are using Switch case instead of if-else
+        int Emp_Hr = 0;
+        int Emp_wage = 0;
+        int Total_wages = 0;
+
+        //In this program we are using Switch case instead of if-else
+        for (int i = 1; i <= Working_Days; i++)
+        {
+            Random obj = new Random();
+            int empcheck = obj.Next(0, 3); //it will generate 0 and 1 only bcoz its limit is n-1 ie 2-1=1
 
             switch (empcheck)
-             {
+            {
                 case Is_Fulltime:
-                    Console.WriteLine("Employee is Fulltime");
                     Emp_Hr = 8;
                     break;
 
                 case Is_Parttime:
-                    Console.WriteLine("Employee is Parttime");
                     Emp_Hr = 4;
                     break;
 
                 default:
-                    Console.WriteLine("Employee is Absent");
                     break;
-             }
+            }
             Emp_wage = Emp_Rate_Hr * Emp_Hr;
-            Console.WriteLine("\nDailywage of current Empolyee is :- " + Emp_wage);
+            Console.WriteLine("Day "+i+" wages:- "+Emp_wage);
+            Total_wages += Emp_wage;
+        }
+            
+        Console.WriteLine("\nWages of current Empolyee for 20 days :- " +Total_wages);
 
 
    }
