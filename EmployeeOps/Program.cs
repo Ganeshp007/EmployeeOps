077796153bd98921973006cfa2eb3afc8b1cec00
Ganeshp007 <potdart40.gp@gmail.com>
2022-04-27 00:06:59 +0530

    Added: UC4_EmpwageSwitchcase

--- EmployeeOps/Program.cs
+++ EmployeeOps/Program.cs
@@ -1,31 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-int Is_Fulltime = 1;
-int Is_Parttime = 2;
-int Emp_Rate_Hr = 20;
-int Emp_Hr = 0;
-int Emp_wage = 0;   
-Random obj=new Random();
-int empcheck=obj.Next(0,3); //it will generate 0 and 1 only bcoz its limit is n-1 ie 2-1=1
-
-Console.WriteLine("----- Welcome to Employee Management System -----\n");
-Console.ReadKey();
-
-if(empcheck == Is_Fulltime)
+class Exception
 {
-    Console.WriteLine("Employee is Fulltime");
-    Emp_Hr = 8;
-}
-else if(empcheck == Is_Parttime)
-{
-    Console.WriteLine("Employee is Parttime");
-    Emp_Hr = 4;
-}
-else
-{
-    Console.WriteLine("Employee is Absent");
-}
+public const int Is_Fulltime = 1;
+public const int Is_Parttime = 2;
+public const int Emp_Rate_Hr = 20;
 
-Emp_wage = Emp_Rate_Hr * Emp_Hr;
-Console.WriteLine("\nDailywage of current Empolyee is :- "+Emp_wage);
+   static void Main(String[] agrs)
+   
+   {
 
+            int Emp_Hr = 0;
+            int Emp_wage = 0;
+            Random obj = new Random();
+            int empcheck = obj.Next(0, 3); //it will generate 0 and 1 only bcoz its limit is n-1 ie 2-1=1
+
+            Console.WriteLine("----- Welcome to Employee Management System -----\n");
+            Console.ReadKey();
+
+            //In this program we are using Switch case instead of if-else
+
+            switch (empcheck)
+             {
+                case Is_Fulltime:
+                    Console.WriteLine("Employee is Fulltime");
+                    Emp_Hr = 8;
+                    break;
+
+                case Is_Parttime:
+                    Console.WriteLine("Employee is Parttime");
+                    Emp_Hr = 4;
+                    break;
+
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    break;
+             }
+            Emp_wage = Emp_Rate_Hr * Emp_Hr;
+            Console.WriteLine("\nDailywage of current Empolyee is :- " + Emp_wage);
+
+
+   }
+
+
+}
